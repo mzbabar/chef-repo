ba4608d752c9edbf493dfbad3a445296bdb8d888
mzbabar <mzbabar@gmail.com>
2014-12-05 11:48:01 -0500

    Updated the files in the the direcory cookbooks

--- cookbooks/learn_chef_httpd/recipes/default.rb
+++ cookbooks/learn_chef_httpd/recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright (c) 2014 The Authors, All Rights Reserved.
+package 'httpd'
+
+service 'httpd' do 
+ action [:start, :enable]
+end
+
+template '/var/www/html/index.html' do 
+    source 'index.html.erb'
+end
+
+service 'iptables' do
+   action :stop
+end
