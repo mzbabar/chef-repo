a6716117d2a3f003fdfeda580b661259cf4d66fb
mzbabar <mzbabar@gmail.com>
2014-12-05 11:32:13 -0500

    Updated  the file webserver.rb

--- webserver.rb
+++ webserver.rb
@@ -1,1 +1,17 @@
 package 'httpd'
+
+service 'httpd' do
+   action [:start, :enable]
+end
+
+file '/var/www/html/index.html' do
+     content '<html>
+         <body>
+             <h1> Hello World</h1>
+         </body>
+     </html>'
+end
+
+service 'iptables' do
+  action :stop
+end
